1cbba528b1a38c8dcf10c05c119eb63f344c22bb
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-03-18 01:25:14 -0400

    Update automático

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,24 @@
 <script setup>
-import { ref, onMounted } from 'vue'
 //import HelloWorld from './components/HelloWorld.vue'
-
+import { ref, reactive, onMounted } from 'vue'
 const musica = ref('')
 const audiosrc = ref('https://radio.somdomato.com/main')
-
 
 onMounted(() => {
   let ts = Math.floor(Date.now() / 1000)
   audiosrc.value = 'https://radio.somdomato.com/main?nc=' + ts 
 
-  //console.log(`The initial count is ${count.value}.`)
-      // mode:"cors",
-
+  setInterval(function () {    
     fetch('https://radio.somdomato.com/json')
       .then(res=>res.json())
       .then((response) => {
-        console.log(response)
-        musica = response.icestats.source.title
+        musica.value.innerText = response.icestats.source.title
       }).catch((error) => {
         console.log('Looks like there was a problem: \n', error);
-      }); 
-
-  // setInterval(function () {
-  // }, 5000);
+      })
+  }, 5000)
 })
-
-//getData();
 </script>
-
 <template>
   <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
@@ -69,32 +59,26 @@ onMounted(() => {
   <main class="flex-shrink-0">
     <div class="container">
       <h1 class="mt-5">Rádio Som do Mato</h1>
-
-       <p class="lead">{{ musica }}</p>
-
-<vue-plyr>
-  <audio controls crossorigin playsinline>
-    <source
-        :src="audiosrc"
-        type="audio/mp3"
-    />
-  </audio>
-</vue-plyr>
-
-
-      <!-- <p>
-        Back to
-        <a href="/docs/5.1/examples/sticky-footer/">the default sticky footer</a> minus the navbar.
-      </p> -->
+       <p class="lead" ref="musica"></p>
+        <vue-plyr>
+          <audio controls crossorigin playsinline>
+            <source
+                :src="audiosrc"
+                type="audio/mp3"
+            />
+          </audio>
+        </vue-plyr>
+        <!-- <p>
+          Back to
+          <a href="/docs/5.1/examples/sticky-footer/">the default sticky footer</a> minus the navbar.
+        </p> -->
     </div>
   </main>
-
   <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
       <span class="text-muted">&copy; 2012-2022 Rádio Som do Mato - A mais sertaneja!</span>
     </div>
   </footer>
-
   <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
 </template>
 
