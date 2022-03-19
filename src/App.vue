030790e40a411b5e3760e7cd3abbdb512f6b497c
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-03-19 10:06:16 -0400

    Update automático

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import PlyrControls from './js/plyr-controls'
 
-//const streamData = ref(null)
-const musica = ref('')
-const ouvintes = ref('')
-const audiosrc = ref('https://radio.somdomato.com/main')
+const ouvintes = ref('0')
+const plyr = ref(null)
 
-const onPausePlyr = _ => {
-  audiosrc.value = 'https://radio.somdomato.com/main?nc=' + Math.floor(Date.now() / 1000)
+function resetSrc() {
+  document.querySelector("source").src = 'https://radio.somdomato.com/main?nc=' + Math.floor(Date.now() / 1000)
+  document.querySelector("audio").load(); //call this to just preload the audio without playing
 }
 
-const fetchData = _ => {
+function onPausePlyr() {
+  resetSrc()
+
+  let playPromise = document.querySelector("audio").play()
+  if (playPromise !== undefined) {
+    playPromise
+      .then(_ => {
+        document.querySelector("audio").play()
+      })
+      .catch(error => { })
+  }
+}
+
+function fetchData() {
   fetch('https://radio.somdomato.com/json')
   .then(res=>res.json())
   .then((response) => {
-    if (musica.value)
-      musica.value.innerText = response.icestats.source.title
-    if (ouvintes.value)
+    document.getElementById("stream-title").innerHTML = response.icestats.source.title
+
+    if (ouvintes.value) {
       ouvintes.value.innerText = response.icestats.source.listeners
+    }
   }).catch((error) => {
     console.log('Looks like there was a problem: \n', error);
   })
 }
 
 onMounted(() => {
-  audiosrc.value = 'https://radio.somdomato.com/main?nc=' + Math.floor(Date.now() / 1000) 
+  document.getElementById("stream-title").innerHTML = 'Rádio Som do Mato'
+
+  document.getElementById("stream-restart").addEventListener("click", function() {
+    onPausePlyr()
+  });
+
+  resetSrc()
   fetchData()
 
   setInterval(function () { 
@@ -36,9 +56,12 @@ onMounted(() => {
   <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Rádio Som do Mato</a>
-        <!-- <button
-          class="navbar-toggler"
+        <a class="navbar-brand" href="#">
+          <img class="m-0 p-0" src="/assets/img/logotipo.svg" alt="Rádio Som do Mato" width="28" height="24">
+          Rádio Som do Mato
+        </a>
+        <button
+          class="navbar-toggler border-0 shadow-none"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarCollapse"
@@ -46,57 +69,50 @@ onMounted(() => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
+
           <span class="navbar-toggler-icon"></span>
+		      <!-- <input type="checkbox" name="main-nav" id="main-nav" class="burger-check"> -->
+		      <!-- <label for="main-nav" class="burger menu"><span></span></label> -->
+
         </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="collapse navbar-collapse show" id="navbarCollapse">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <!-- <li class="nav-item">
+              <a class="nav-link disabled">A mais sertaneja!</a>
+            </li> -->
+          </ul>    
+          
+          <ul class="navbar-nav mb-2 mb-md-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link disabled" id="stream-title">Carregando...</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div> -->
+          </ul>   
+
+          <div class="plyr__container">
+            <vue-plyr ref="plyr" :options="{ controls: PlyrControls }">
+              <audio @pause="resetSrc" controls crossorigin playsinline>
+                <source src="https://radio.somdomato.com/main" type="audio/mp3" />
+              </audio>
+            </vue-plyr>
+          </div>
+
+        </div>
       </div>
     </nav>
   </header>
-
-  <main class="flex-shrink-0">
-    <div class="container">
-      <h1 class="mt-5">Rádio Som do Mato</h1>
-       <p class="lead" ref="musica">Carregando...</p>
-        <vue-plyr>
-          <audio @pause='onPausePlyr' controls crossorigin playsinline>
-            <source
-                :src="audiosrc"
-                type="audio/mp3"
-            />
-          </audio>
-        </vue-plyr>
-        <!-- <p>
-          Back to
-          <a href="/docs/5.1/examples/sticky-footer/">the default sticky footer</a> minus the navbar.
-        </p> -->
+  <main class="h-100 d-flex align-items-center">
+    <div class="container text-center">
+      <img src="/assets/img/logo.png" class="img-fluid" alt="...">
     </div>
   </main>
   <footer class="footer mt-auto py-3">
     <div class="container">
       <p class="text-muted m-0">&copy; 2012-2022 Rádio Som do Mato</p>
-      <p class="text-muted m-0">A mais sertaneja!</p>
+      <p class="text-muted m-0">Por amor à música sertaneja</p>
       <p class="text-muted m-0 small">Ouvintes: <span ref="ouvintes">0</span></p>
     </div>
   </footer>
-  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
 </template>
-
 <style>
 @import "./scss/app.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VuePlyr from '../node_modules/@skjnldsv/vue-plyr'
+// import PlyrControls from './js/plyr-controls'
 import 'bootstrap'
 
 createApp(App)
     .use(VuePlyr, { 
-        plyr: { controls: ['play', 'mute', 'volume'] } 
+        plyr: { 
+        } 
     })
     .mount('#app')
